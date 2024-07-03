422f3fa004aa09a2245706e1d019333dc2103311
BohdanObruch <bodan1992@gmail.com>
2024-07-03 09:28:44 +0300

    fixed random count and test "Add and verify items"

--- src/tests/shopping_cart.spec.js
+++ src/tests/shopping_cart.spec.js
@@ -28,33 +28,3 @@ test.describe('Adding random items to the shopping cart', () => {
         expect(inventoryItemsPrices).toEqual(shoppingCartItemsPrices);
     });
 });
-const { expect } = require('@playwright/test');
-const { test } = require('../fixture');
-const { randomChoiceItems } = require('../utils/choice_random_items');
-const { standardUser } = require('../../config/credentials');
-
-test.describe('Adding random items to the shopping cart', () => {
-    test('Add and verify items', async ({ inventoryPage, loginPage, baseSwagLabPage }) => {
-        await loginPage.navigate();
-        await loginPage.performLogin(standardUser.username, standardUser.password);
-
-        const allItems = await inventoryPage.getInventoryItems();
-        const randomItems = await randomChoiceItems(allItems);
-
-        await inventoryPage.addItemsToCart(randomItems);
-
-        const cartItemsNames = await inventoryPage.getInventoryItemsNames();
-        const cartItemsDescriptions = await inventoryPage.getInventoryItemsDescriptions();
-        const inventoryItemsPrices = await inventoryPage.getInventoryItemsPrices();
-
-        await baseSwagLabPage.clickOnShopingCart;
-
-        const shoppingCartItemsNames = await inventoryPage.getInventoryItemsNames();
-        const shoppingCartItemsDescriptions = await inventoryPage.getInventoryItemsDescriptions();
-        const shoppingCartItemsPrices = await inventoryPage.getInventoryItemsPrices();
-
-        expect(cartItemsNames).toEqual(shoppingCartItemsNames);
-        expect(cartItemsDescriptions).toEqual(shoppingCartItemsDescriptions);
-        expect(inventoryItemsPrices).toEqual(shoppingCartItemsPrices);
-    });
-});
